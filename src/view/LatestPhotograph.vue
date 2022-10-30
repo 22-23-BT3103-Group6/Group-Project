919d6e7125b6706671631929716d059c7b2ed2d2
yeyi-alice <92208762+yeyi-alice@users.noreply.github.com>
2022-10-30 14:06:28 +0800

    i

--- src/view/LatestPhotograph.vue
+++ src/view/LatestPhotograph.vue
@@ -1,13 +1,19 @@
 <template>
+  <HeadLine/>
   <h2>Latest Photograph</h2>
 </template>
 
 <script>
-export default {
+import HeadLine from '@/components/HeadLine.vue'
 
+export default{
+    name: "LatestPhoto",
+    components:{
+        HeadLine,
+    },
 }
 </script>
-
+  
 <style>
 
 </style>
